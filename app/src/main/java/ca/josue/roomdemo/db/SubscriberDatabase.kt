4de68dbd3c6bbe2e058306cee3d3ed2f317aa2b6
Josue Lubaki <josuelubaki@gmail.com>
2022-12-26 20:25:50 -0500

    config schema auto migration - Room

--- app/src/main/java/ca/josue/roomdemo/db/SubscriberDatabase.kt
+++ app/src/main/java/ca/josue/roomdemo/db/SubscriberDatabase.kt
@@ -1,11 +1,17 @@
 package ca.josue.roomdemo.db
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Subscriber::class], version = 1)
+@Database(
+    entities = [Subscriber::class],
+    version = 1,
+    exportSchema = true,
+    autoMigrations = [AutoMigration(from = 1, to = 2)]
+)
 abstract class SubscriberDatabase : RoomDatabase() {
 
     abstract val subscriberDAO: SubscriberDAO
